5559f8e344a5c21f0d9f4851a4cd3c8aa7f435d4
maty <matyoliva337@gmail.com>
2018-05-27 12:17:30 -0300

    test

--- tg_bot/modules/calendar.py
+++ tg_bot/modules/calendar.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-import telebot
-from telebot import types
-import datetime
-from telegramcalendar import create_calendar
 from tg_bot import dispatcher
 
 bot = telebot.TeleBot("")
