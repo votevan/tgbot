bbaf7c4d7f1713273e838f166bc6805e2179a0ef
maty <matyoliva337@gmail.com>
2018-06-11 15:32:44 -0300

    Traducido. Incluye traducciones originales.

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -31,32 +31,47 @@ def purge(bot: Bot, update: Update, args: List[str]) -> str:
                     bot.deleteMessage(chat.id, m_id)
                 except BadRequest as err:
                     if err.message == "Message can't be deleted":
-                        bot.send_message(chat.id, "No pude borrar todos los mensajes. Los mensajes pueden ser muy viejos, podría "
-                                                  "no tener permisos, o esto puede no ser un supergrupo.")
+                        bot.send_message(chat.id, "No pude borrar todos los mensajes: pueden ser muy viejos, podría "
+                                                  "no tener permisos o esto no es un supergrupo.")
+
+                                                  #Original:
+                                                  #Cannot delete all messages. The messages may be too old, I might
+                                                  #not have delete rights, or this might not be a supergroup.
 
                     elif err.message != "Message to delete not found":
-                        LOGGER.exception("Error while purging chat messages.")
+                        LOGGER.exception("Error al eliminar mensajes de chat.") #Original: Error while purging chat messages.
 
             try:
                 msg.delete()
             except BadRequest as err:
                 if err.message == "Message can't be deleted":
-                    bot.send_message(chat.id, "No pude borrar todos los mensajes. Los mensajes pueden ser muy viejos, podría "
-                                              "no tener permisos, o esto puede no ser un supergrupo.")
+
+                    bot.send_message(chat.id, "No pude borrar todos los mensajes: pueden ser muy viejos, podría "
+                                              "no tener permisos o esto no es un supergrupo.")
+
+                                              #Original:
+                                              #Cannot delete all messages. The messages may be too old, I might
+                                              #not have delete rights, or this might not be a supergroup.
 
                 elif err.message != "Message to delete not found":
-                    LOGGER.exception("Error while purging chat messages.")
+                    LOGGER.exception("Error al eliminar mensajes de chat.") #Original: Error while purging chat messages.
 
-            bot.send_message(chat.id, "Eliminación correcta.")
+            bot.send_message(chat.id, "Eliminación completada.") #Original: Purge complete.
             return "<b>{}:</b>" \
                    "\n#PURGE" \
-                   "\n<b>Admin:</b> {}" \
-                   "\nPurged <code>{}</code> messages.".format(html.escape(chat.title),
+                   "\n<b>Administrador:</b> {}" \
+                   "\nEliminó <code>{}</code> mensajes.".format(html.escape(chat.title),
                                                                mention_html(user.id, user.first_name),
                                                                delete_to - message_id)
 
+                   #Original:
+                   #\n#PURGE
+                   #\n<b>Admin:</b> {}
+                   #\nPurged <code>{}</code> messages.
+
     else:
-        msg.reply_text("Responda a un mensaje para seleccionar desde dónde comenzar a eliminar.")
+        msg.reply_text("Respondé a un mensaje para seleccionar desde dónde comenzar a eliminar.")
+                       #Original: Reply to a message to select where to start purging from.
 
     return ""
 
@@ -73,11 +88,16 @@ def del_message(bot: Bot, update: Update) -> str:
             update.effective_message.delete()
             return "<b>{}:</b>" \
                    "\n#DEL" \
-                   "\n<b>Admin:</b> {}" \
-                   "\nMessage deleted.".format(html.escape(chat.title),
+                   "\n<b>Administrador:</b> {}" \
+                   "\nMensaje borrado.".format(html.escape(chat.title),
                                                mention_html(user.id, user.first_name))
+
+                   #Original:
+                   #\n#DEL" \
+                   #\n<b>Admin:</b> {}" \
+                   #\nMessage deleted.
     else:
-        update.effective_message.reply_text("Qué quieres borrar?")
+        update.effective_message.reply_text("¿Qué querés borrar?") #Original: Whadya want to delete?
 
     return ""
 
@@ -89,7 +109,13 @@ __help__ = """
  - /purge <valor>: borra el mensaje respondido y los X mensajes que le siguen.
 """
 
-__mod_name__ = "Eliminación"
+#Original:
+#*Admin only:*
+# - /del: deletes the message you replied to
+# - /purge: deletes all messages between this and the replied to message.
+# - /purge <integer X>: deletes the replied message, and X messages following it.
+
+__mod_name__ = "Eliminación" #Original: Purges
 
 DELETE_HANDLER = CommandHandler("del", del_message, filters=Filters.group)
 PURGE_HANDLER = CommandHandler("purge", purge, filters=Filters.group, pass_args=True)
