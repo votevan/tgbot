d4f8bdfcda209631feae5b02959b2036ce9001d1
Maty Oliva <matyoliva337@gmail.com>
2018-07-04 19:38:07 -0300

    Traducido.

--- tg_bot/modules/userinfo.py
+++ tg_bot/modules/userinfo.py
@@ -29,11 +29,12 @@ def about_me(bot: Bot, update: Update, args: List[str]):
                                             parse_mode=ParseMode.MARKDOWN)
     elif message.reply_to_message:
         username = message.reply_to_message.from_user.first_name
-        update.effective_message.reply_text(username + " no ha establecido un mensaje de información sobre ellos todavia!")
-#Orignal: " hasn't set an info message about themselves  yet!"
+        update.effective_message.reply_text(username + "no la puso, me refiero a la bio")
+                                           #Original: username + "hasn't set an info message about themselves  yet!"
     else:
-        update.effective_message.reply_text("¡Aún no estableciste un mensaje de información sobre ti!")
-#Original: "You haven't set an info message about yourself yet!"
+        update.effective_message.reply_text("todavia no ponés la bio master)
+                                           #Original: You haven't set an info message about yourself yet!
+
 
 @run_async
 def set_about_me(bot: Bot, update: Update):
@@ -44,12 +45,12 @@ def set_about_me(bot: Bot, update: Update):
     if len(info) == 2:
         if len(info[1]) < MAX_MESSAGE_LENGTH // 4:
             sql.set_user_me_info(user_id, info[1])
-            message.reply_text("¡Tu info esta actualizada!")
-#Original: "Updated your info!"
+            message.reply_text("Perfecto.") #Original: Updated your info!
         else:
             message.reply_text(
-                "¡Tu info tiene que tener menos de {} caracteres! Tienes {}.".format(MAX_MESSAGE_LENGTH // 4, len(info[1])))
-#Original: "Your info needs to be under {} characters! You have {}."
+                "no máquina son al menos {} caracteres y tenés {}.".format(MAX_MESSAGE_LENGTH // 4, len(info[1])))
+                #Original: Your info needs to be under {} characters! You have {}.
+
 
 @run_async
 def about_bio(bot: Bot, update: Update, args: List[str]):
@@ -68,11 +69,11 @@ def about_bio(bot: Bot, update: Update, args: List[str]):
                                             parse_mode=ParseMode.MARKDOWN)
     elif message.reply_to_message:
         username = user.first_name
-        update.effective_message.reply_text("¡{} no estableció un conjunto de mensaje sobre ellos todavía!".format(username))
-#Original: "{} hasn't had a message set about themselves yet!"
+        update.effective_message.reply_text("{} es un sujeto de pocas palabras".format(username))
+                                            #Original: {} hasn't had a message set about themselves yet!
     else:
-        update.effective_message.reply_text("¡No estableciste un conjunto de biografía sobre tí todavía!")
-#Original: "You haven't had a bio set about yourself yet!"
+        update.effective_message.reply_text("aún no tenés una bio salame")
+                                            #Original: You haven't had a bio set about yourself yet!
 
 
 @run_async
@@ -83,12 +84,12 @@ def set_about_bio(bot: Bot, update: Update):
         repl_message = message.reply_to_message
         user_id = repl_message.from_user.id
         if user_id == message.from_user.id:
-            message.reply_text("Ha, ¡No puedes establecer tu propia biografía! Estaś a merced de otros aquí...")
-#Original: "Ha, you can't set your own bio! You're at the mercy of others here..."
+            message.reply_text("no podés establecer tu propia biografía xdd")
+                              #Original: Ha, you can't set your own bio! You're at the mercy of others here...
             return
         elif user_id == bot.id and sender.id not in SUDO_USERS:
-            message.reply_text("Emm... se, yo solo confío en los administradores para cambiar mi biografía.")
-#Original: "Erm... yeah, I only trust sudo users to set my bio."
+            message.reply_text("kaces solo confío en los sudo para configurar mi bio")
+                              #Original: Erm... yeah, I only trust sudo users to set my bio.
             return
 
         text = message.text
@@ -96,49 +97,48 @@ def set_about_bio(bot: Bot, update: Update):
         if len(bio) == 2:
             if len(bio[1]) < MAX_MESSAGE_LENGTH // 4:
                 sql.set_user_bio(user_id, bio[1])
-                message.reply_text("¡Actualizado la biografía de {}!".format(repl_message.from_user.first_name))
-#Original: Updated {}'s bio!
+                message.reply_text("listo máquina, actualicé la bio de {}".format(repl_message.from_user.first_name))
+                                   #Original: Updated {}'s bio!
             else:
                 message.reply_text(
-                    "Una biografía debe tener menos de {} caracteres! Trataste de poner {}.".format(
+                    "necesitas {} caracteres para la bio e intentaste poner {}".format(
+                    #Original: A bio needs to be under {} characters! You tried to set {}.
                         MAX_MESSAGE_LENGTH // 4, len(bio[1])))
-#Original: "A bio needs to be under {} characters! You tried to set {}."
     else:
-        message.reply_text("¡Responde al mensaje de alguien para editar su biografía!")
-#Original: "Reply to someone's message to set their bio!"
+        message.reply_text("no master, respondele el mensaje")
+                           #Original: Reply to someone's message to set their bio!"
 
 
 def __user_info__(user_id):
     bio = html.escape(sql.get_user_bio(user_id) or "")
     me = html.escape(sql.get_user_me_info(user_id) or "")
     if bio and me:
-        return "<b>Acerca del usuario:</b>\n{me}\n<b>Qué dicen los demás:</b>\n{bio}".format(me=me, bio=bio)
-#Original: "<b>About user:</b>\n{me}\n<b>What others say:</b>\n{bio}"
+        return "<b>Acerca de este man:</b>\n{me}\n<b>Lo que dicen:</b>\n{bio}".format(me=me, bio=bio)
+               #Original: <b>About user:</b>\n{me}\n<b>What others say:</b>\n{bio}
     elif bio:
-        return "<b>Qué dicen los demás:</b>\n{bio}\n".format(me=me, bio=bio)
-#Original: "<b>What others say:</b>\n{bio}\n"
+        return "<b>Lo que dicen:</b>\n{bio}\n".format(me=me, bio=bio)
+               #Original: <b>What others say:</b>\n{bio}\n
     elif me:
-        return "<b>Acerca del usuario:</b>\n{me}""".format(me=me, bio=bio)
-#Original "<b>About user:</b>\n{me}"""
+        return "<b>Acerca de este man:</b>\n{me}""".format(me=me, bio=bio)
+               #Original: <b>About user:</b>\n{me}
     else:
         return ""
 
 
 __help__ = """
- - /setbio <texto>: mientras respondas, guarda la biografía de otro usuario.
- - /bio: Retorna tu biografía o la de otro usuario. No puede ser editado por tí.
- - /setme <texto>: Guarda tu información.
- - /me: Retorna tu info o la de otro usuario.
+- /setbio <texto>: cuando respondas se va a guardar la bio del otro pibe
+- /bio: esto es para obtener tu bio o la de otro wacho
+- /setme <texto>: configurá tu bio
+- /me: extraé la bio tuya o de otra persona
 """
 
-#Original: """
+#Original:
 # - /setbio <text>: while replying, will save another user's bio
 # - /bio: will get your or another user's bio. This cannot be set by yourself.
 # - /setme <text>: will set your info
 # - /me: will get your or another user's info
-#"""
 
-__mod_name__ = "Bios and Abouts"
+__mod_name__ = "Bios"
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio)
 GET_BIO_HANDLER = DisableAbleCommandHandler("bio", about_bio, pass_args=True)
