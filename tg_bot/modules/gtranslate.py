b55b90f329488bb6d7ac64082fe337d07f4d6b2d
Maty Oliva <matyoliva337@gmail.com>
2018-08-21 15:25:56 -0300

    test

---
+++ tg_bot/modules/gtranslate.py
@@ -0,0 +1,16 @@
+from telegram import Message, Update, Bot, User
+from telegram.ext import Filters, MessageHandler, run_async
+from googletrans impor Translator
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot import dispatcher
+
+@run_async
+def gtranslate(bot: Bot, update: Update):
+  message = update.effective_message
+  text = message.reply_to_message.text
+  reply_text=translator.translate(text, dest='en').text
+  reply_text="`Fuente: `\n"+text+"`Traduccion: `\n"+reply_text
+  message.reply_to_message.reply_text(reply_text)
+
+gtranslate_handler = DisableAbleCommandHandler("gtranslate", gtranslate)
+dispatcher.add_handler(gtranslate_handler)
---
+++ tg_bot/modules/wikipedia.py
@@ -0,0 +1,15 @@
+import wikipedia
+from telegram import Update, Bot
+from telegram.ext import run_async, Filters
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+def wiki(bot: Bot, update: Update):
+        query = update.effective_message.text.split(None, 1)
+        result = '**Busqueda:**\n`' + query + '`\n\n**Resultado:**\n`' + wikipedia.summary(match)
+        update.effective_message.reply_markdown(result)
+__help__ = """
+ - /wiki: Busca en Wikipedia.
+ """
+__mod_name__ = "Wikipedia"
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
+dispatcher.add_handler(WIKI_HANDLER)
