647215b90dd4c4bb8cfb9fb58793255357322402
Maty Oliva <matyoliva337@gmail.com>
2018-08-26 19:27:47 -0300

    fix

--- tg_bot/modules/wikipedia.py
+++ tg_bot/modules/wikipedia.py
@@ -3,9 +3,11 @@ from telegram import Update, Bot
 from telegram.ext import run_async, Filters
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
+
 def wiki(bot: Bot, update: Update):
         query = update.effective_message.text.split(None, 1)
-        result = '**Search:**\n`' + str(query) + '`\n\n**Result:**\n`' + wikipedia.summary(match)
+        match = wikipedia.summary
+        result = '**Search:**\n`' + str(query) + '`\n\n**Result:**\n`' + match
         update.effective_message.reply_markdown(result)
 __help__ = """
  - /wiki: Query the Wikipedia
@@ -13,3 +15,23 @@ __help__ = """
 __mod_name__ = "Wikipedia Search"
 WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
 dispatcher.add_handler(WIKI_HANDLER)
+
+
+
+
+
+#def google(bot: Bot, update: Update):
+#        query = update.effective_message.text.split(None, 1)
+#        result_ = subprocess.run(['gsearch', str(query)], stdout=subprocess.PIPE)
+#        result = str(result_.stdout.decode())
+#        update.effective_message.reply_markdown('ℹ️ Búsqueda: ' + str(query) + '\nℹ️ Resultados:\n' + result)
+#
+#__help__ = """
+# - /google: Google search
+# """
+
+#__mod_name__ = "Google"
+
+#GOOGLE_HANDLER = DisableAbleCommandHandler("google", google)
+
+#dispatcher.add_handler(GOOGLE_HANDLER)
