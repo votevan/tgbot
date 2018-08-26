0b922febc32febef195c395c4f2e40fae2eadba8
Maty Oliva <matyoliva337@gmail.com>
2018-08-26 19:34:09 -0300

    fix

--- tg_bot/modules/wikipedia.py
+++ tg_bot/modules/wikipedia.py
@@ -5,13 +5,14 @@ from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
 
 def wiki(bot: Bot, update: Update):
-        query = update.effective_message.text.split(None, 1) 
+        query = update.effective_message.text.split(None, 1)
+
         result = '**Search:**\n`' + str(query) + '`\n\n**Result:**\n`' + str(match)
         update.effective_message.reply_markdown(result)
-__help__ = """
- - /wiki: Query the Wikipedia
- """
-__mod_name__ = "Wikipedia Search"
+#__help__ = """
+ #- /wiki: Query the Wikipedia
+ #"""
+#__mod_name__ = "Wikipedia Search"
 WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
 dispatcher.add_handler(WIKI_HANDLER)
 
