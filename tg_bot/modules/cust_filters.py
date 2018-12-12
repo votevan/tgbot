618abc00114e845caa0845984724608aedf6d0e2
Maty Oliva <matyoliva337@gmail.com>
2018-12-12 18:57:17 -0300

    test

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -153,17 +153,17 @@ def reply_filter(bot: Bot, update: Update):
         if re.search(pattern, to_match, flags=re.IGNORECASE):
             filt = sql.get_filter(chat.id, keyword)
             if filt.is_sticker:
-                message.reply_to_message.reply_sticker(filt.reply)
+                message.reply_sticker(filt.reply)
             elif filt.is_document:
-                message.reply_to_message.reply_document(filt.reply)
+                message.reply_document(filt.reply)
             elif filt.is_image:
-                message.reply_to_message.reply_photo(filt.reply)
+                message.reply_photo(filt.reply)
             elif filt.is_audio:
-                message.reply_to_message.reply_audio(filt.reply)
+                message.reply_audio(filt.reply)
             elif filt.is_voice:
-                message.reply_to_message.reply_voice(filt.reply)
+                message.reply_voice(filt.reply)
             elif filt.is_video:
-                message.reply_to_message.reply_video(filt.reply)
+                message.reply_video(filt.reply)
             elif filt.has_markdown:
                 buttons = sql.get_buttons(chat.id, filt.keyword)
                 keyb = build_keyboard(buttons)
