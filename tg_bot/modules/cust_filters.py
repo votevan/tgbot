e012b2b4f2d9bcb7ed392f457fbe486e61109262
Maty Oliva <matyoliva337@gmail.com>
2018-12-15 16:04:49 -0300

    test commit

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -153,7 +153,7 @@ def reply_filter(bot: Bot, update: Update):
         if re.search(pattern, to_match, flags=re.IGNORECASE):
             filt = sql.get_filter(chat.id, keyword)
             if filt.is_sticker:
-                message.reply_sticker(filt.reply)
+                message.reply_to_message and message.reply_sticker(filt.reply)
             elif filt.is_document:
                 message.reply_document(filt.reply)
             elif filt.is_image:
